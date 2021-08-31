2f3ed9313fadaf60ee193aa9b04aa235ef31945f
Lavis-log <jminrh@gmail.com>
2021-08-31 17:26:20 +0900

    testing 2

--- training-newidrecommend/test.js
+++ training-newidrecommend/test.js
@@ -1,16 +1,26 @@
 function solution(new_id) {
     document.write(new_id + '<br>');
     var answer = '';
-    var id3 = '';
+    var idCheck3 = '';
+    var temp ='';
     var reg = /[\{\}\[\]\/?,;:|\)*~`!^\+<>@\#$%&\\\=\(\'\"]/gi
-    var id = String(new_id);
-    var id2 = id.toLowerCase().replace(reg,'');
-    document.write(id2.match(/\./g).length + '<br>');
-    for (var i=0; i<id2.match(/\./g).length; i++){ 
-        id2.replace(/\.\./g,'.');
+    var idCheck1 = String(new_id);
+    var idCheck2 = idCheck1.toLowerCase().replace(reg,'');
+    document.write(idCheck2.match(/\./g).length + '<br>');
+    for (var i=0; i<idCheck2.match(/\./g).length; i++){ 
+        idCheck3 = idCheck2.replace(/\.\./g,'.');
+        temp = idCheck3;
+        idCheck2 = idCheck3;
     };
-    document.write(id2 + '<br>');
-    answer = id2.replace(/^\./,'').replace(/\.$/,'').replace(/\s/g,'a');
+    document.write(idCheck3 + '<br>');
+    var idCheck4 = idCheck3.replace(/^\./,'').replace(/\.$/,'')
+    var idCheck5 = idCheck4.replace(null, "a");
+    temp = '';
+    if (idCheck5.length >= 16){
+        var idCheck6 = idCheck5.slice(0,15);
+        
+    }
+    document.write(idCheck6+ '<br>');
     document.write(answer);
     return answer;
-}
+}   
