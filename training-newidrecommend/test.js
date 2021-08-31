a9335a41c866233fb255c28dead426b3445ea323
Lavis-log <jminrh@gmail.com>
2021-08-31 17:31:03 +0900

    testing 2

--- training-newidrecommend/test.js
+++ training-newidrecommend/test.js
@@ -12,13 +12,19 @@ function solution(new_id) {
         temp = idCheck3;
         idCheck2 = idCheck3;
     };
-    document.write(idCheck3 + '<br>');
+    document.write('idCheck3: ' + idCheck3 + '<br>');
     var idCheck4 = idCheck3.replace(/^\./,'').replace(/\.$/,'')
+    document.write('idCheck4: ' + idCheck4 + '<br>');
     var idCheck5 = idCheck4.replace(null, "a");
+    document.write('idCheck5: ' + idCheck5 + '<br>');
     temp = '';
     if (idCheck5.length >= 16){
-        var idCheck6 = idCheck5.slice(0,15);
-        
+        temp = idCheck5.slice(0,15);
+        if (temp.length === null){
+            var idCheck6 = idCheck5;
+        }else{
+            var idCheck6 = temp;
+        }
     }
     document.write(idCheck6+ '<br>');
     document.write(answer);
