8995371788d6ffba9e390af3b22196702cd23ab5
Lavis-log <jminrh@gmail.com>
2021-09-01 17:40:55 +0900

    this problem is toooooooo difficult. I have to study with map, reduce, etc.

--- training-clawmachine/clawmachine.js
+++ training-clawmachine/clawmachine.js
@@ -1,17 +1,47 @@
 function solution(board, moves) {
     var answer = 0;
-    
-    
-    for( var i = 0; i <= 4; i++) {
-        document.write(board[i] + '<br>');
-        //document.write(board[3,4]);
+    let choicesCount = 0;
+    let result = [];
+    for (let i=0; i < moves.length; i++){
+        for (let j=0; j < board.length; j++){
+            document.write(board[j][moves[i]-1] + '<tr>');
+            if (board[j][moves[i]-1] !== 0 && choicesCount === i){
+                choicesCount++;
+                if (result[result.length - 1] === board[j][moves[i]-1]){
+                    result.pop();
+                    answer += 2;
+                } else result.push(board[j][moves[i] - 1]);
+                board[j][moves[i] - 1] = 0;
+            }
+        }
     }
-    for( var i = 0; i <= 4 ; i++) {
-        document.write(board[i][i-1] + '<br>');
-
-        //document.write(board[3,4]);
-    }
-    document.write(board.pop([4][3]));
-    
     return answer;
 }
+
+// must to study : map, reduce, =>, ...
+const transpose = matrix =>
+    matrix.reduce(
+        (result, row) => row.map((_, i) => [...(result[i] || []), row[i]]),
+        []
+    );
+
+const solution2 = (board, moves) => {
+    const stacks = transpose(board).map(row =>
+        row.reverse().filter(el => el !== 0)
+    );
+    const basket = [];
+    let result = 0;
+
+    for (const move of moves) {
+        const pop = stacks[move - 1].pop();
+        if (!pop) continue;
+        if (pop === basket[basket.length - 1]) {
+            basket.pop();
+            result += 2;
+            continue;
+        }
+        basket.push(pop);
+    }
+
+    return result;
+};
