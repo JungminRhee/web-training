f3ee7087fcba082770faecbd029c15c95993e2ff
Lavis-log <jminrh@gmail.com>
2021-09-09 13:04:26 +0900

    study

--- training-study/study.js
+++ training-study/study.js
@@ -21,21 +21,37 @@
 // }
 // console.log(factoial(10));
 
-const person = {
-    name: ['Bob', 'Smith'],
-    age: 32,
-    gender: 'male',
-    interests: ['music', 'skiing'],
-    bio: function() {
-        console.log(this.name[0] + ' ' +this.name[1] + ' is ' +this.age + ' years old. He likes ' + this.interests[0] + ' and ' + this.interests[1] +'.');
-    },
-    greeting: function() {
-        console.log('Hi! I\'m ' + this.name[0] + '.');
-    }
-};
+// const person = {
+//     name: ['Bob', 'Smith'],
+//     age: 32,
+//     gender: 'male',
+//     interests: ['music', 'skiing'],
+//     bio: function() {
+//         console.log(this.name[0] + ' ' +this.name[1] + ' is ' +this.age + ' years old. He likes ' + this.interests[0] + ' and ' + this.interests[1] +'.');
+//     },
+//     greeting: function() {
+//         console.log('Hi! I\'m ' + this.name[0] + '.');
+//     }
+// };
 
-person['eyes'] = 'hazel';
-person['eyes'];
-person.bio();
-person.greeting();
-console.log(person);
+// person['eyes'] = 'hazel';
+// person['eyes'];
+// person.bio();
+// person.greeting();
+// console.log(person);
+
+// var myDataName = 'height';
+// var myDataValue = '1.75m';
+// person[myDataName] = myDataValue;
+
+// console.log(person.height);
+
+function Person(name) {
+    this.name = name;
+    this.greeting = function(){
+        console.log('Hi! I\'m ' + this.name + '.');
+    };
+}
+
+var person1 = new Person('Bob');
+person1.greeting();
