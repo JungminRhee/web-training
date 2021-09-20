945e320ab98e064781e8221e58e2bf9dd7478cb7
Jungmin Rhee <jminrh@gmail.com>
2021-09-20 14:35:52 +0900

    2021.09.20

--- training-nonplus/numbercard.js
+++ training-nonplus/numbercard.js
@@ -1,18 +1,48 @@
 function solution(s) {
-  var answer = 0;
-  const number = {
-    zero: 0,
-    one: 1,
-    two: 2,
-    three: 3,
-    four: 4,
-    five: 5,
-    six: 6,
-    seven: 7,
-    eight: 8,
-    nine: 9,
-  };
-  s.filter();
-  return answer;
+  return Number(
+    s //
+      .replace(/zero/gi, "0")
+      .replace(/one/gi, "1")
+      .replace(/two/gi, "2")
+      .replace(/three/gi, "3")
+      .replace(/four/gi, "4")
+      .replace(/five/gi, "5")
+      .replace(/six/gi, "6")
+      .replace(/seven/gi, "7")
+      .replace(/eight/gi, "8")
+      .replace(/nine/gi, "9")
+  );
 }
-console.log(solution("one4seveneight"));
+
+// 정규식 말고 깔끔하게 하는법
+function solution2(s) {
+  let numbers = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+  ];
+  var answer = s;
+
+  // 숫자를 인덱스를 활용해야겠다!! 까지 생각하고 그 이후 아이디어가 생각 안난게 아쉽다 ㅠ
+  for (let i = 0; i < numbers.length; i++) {
+    // numbers.length로 체크하는 이유: 0부터 9까지 문자로 된걸 다 봐야되니깐
+    let arr = answer.split(numbers[i]); // numbers[i] 기준으로 만약 one 이라면 걔를 기준으로 one빼고 문자열을 쪼갬
+    console.log(`i: ${i}, arr: ${arr}, answer: ${answer}`);
+    answer = arr.join(i); // one 대신 1을 인수로 넣어서 합치기! 그럼 one -> 1로 바뀐다.
+    console.log(answer);
+  }
+
+  return Number(answer);
+}
+console.log(solution2("one4seveneight"));
+console.log(solution2("2three45sixseven"));
+console.log(solution2("23four5six7"));
+console.log(solution2("123"));
+console.log(solution2("oneoneoneone")); // 이 경우에는 one이 i = 1일때 한번에 다 바뀌지만 i=9까지 끝까지 체크~
